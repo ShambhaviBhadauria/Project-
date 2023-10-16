86dbdbcbe4a8d2efa74651f699dce1e83279dcb3
Shambhavi <107373811+Shamy152@users.noreply.github.com>
2023-10-17 12:28:34 +1300

    updating server

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const app = express();
 
-mongoose.connect('mongodb+srv://ense701:zddX9q9f8F9mUf9C@ense701.lu4fcfq.mongodb.net/ense701', {
+mongoose.connect('mongodb+srv://ense701:zddX9q9f8F9mUf9C@ense701.lu4fcfq.mongodb.net/backend', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
@@ -19,6 +19,36 @@ const ArticleSchema = new mongoose.Schema({
 });
 
 const ArticleModel = mongoose.model('articles', ArticleSchema);
+
+const ArticleSchema = new mongoose.Schema({
+  title :{
+    type: String
+},
+authorname :{
+  type: String
+},
+yearOfPublication :{
+  type: String
+},
+volume :{
+  type: Number
+},
+extraInformation :{
+  type: String
+},
+responseBool :{
+  type: Boolean
+},
+responseEmail :{
+  type: string
+},
+
+
+
+
+})
+
+const collection = new mongoose.model('Articles', ArticleSchema) // creates a collection which uses the article schema 
 
 app.get('/getArticles', (req, res) => {
   ArticleModel.find({})
